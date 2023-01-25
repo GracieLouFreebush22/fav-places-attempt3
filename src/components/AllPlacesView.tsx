9c2231ceb7bb506fbbfead1d993b957391dc65d6
grace k <grace.kuperman5472@gmail.com>
2023-01-25 22:56:55 +0000

    lookin good

--- src/components/AllPlacesView.tsx
+++ src/components/AllPlacesView.tsx
@@ -1,21 +1,35 @@
 import { myPlaces } from "./PlaceData";
+import "./AllPlacesView.css";
 
 function AllPlacesView(): JSX.Element {
   return (
     <div className="body">
       {" "}
-      Grace's Favorite Places
-      {myPlaces.map((placeItem, i) => {
-        return (
-          <div key={i}>
-            <div>
-              <h2> {placeItem.title}</h2>
-              <h3> {placeItem.location}</h3>
-              <img src={placeItem.img} alt="" width="500px" height="500px" />
+      <div className="ctn-header">
+        <h1> My Favorite Places</h1>
+      </div>
+      <div className="ctn-mappedPlaces">
+        {myPlaces.map((placeItem, i) => {
+          return (
+            <div key={i}>
+              <div className="ctn-place">
+                <div className="ctn-place-header">
+                  <h2> {placeItem.title}</h2>
+                  <h3> {placeItem.location}</h3>
+                </div>
+                <div className="ctn-img">
+                  <img
+                    src={placeItem.img}
+                    alt=""
+                    width="800px"
+                    height="500px"
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
